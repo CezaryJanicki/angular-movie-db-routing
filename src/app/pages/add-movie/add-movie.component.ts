3d622a044b14c92f970f5e9ae7e2fcac946dec43
Cezary <cezary.janicki@gmail.com>
2021-12-28 12:51:26 +0100

    Final version with validation

--- src/app/pages/add-movie/add-movie.component.ts
+++ src/app/pages/add-movie/add-movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Movie} from '../../models/movie';
+import { NgModel} from '@angular/forms';
+import {HttpService} from '../../services/http.service';
+import {HttpMoviesService} from '../../services/http-movies.service';
 
 @Component({
   selector: 'app-add-movie',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMovieComponent implements OnInit {
 
-  constructor() { }
+  model: Partial<Movie> = {};
+  categories: string[] = [];
+  years: string[] = [];
+  constructor(private http: HttpService, private httpMoviesService: HttpMoviesService) { }
 
   ngOnInit(): void {
+    this.http.getCategories().subscribe(categories => this.categories = categories);
+    this.http.getYears().subscribe(years => this.years = years)
   }
 
+  send() {
+    console.log(this.model);
+    this.httpMoviesService.postMovie(this.model as Movie).subscribe(
+      result => console.log(result),
+      error => console.error(error)
+    );
+  }
+
+  printModel(title: NgModel) {
+    console.log(title);
+  }
 }
